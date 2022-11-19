fd65e10a2e8ef7db754594f6d046f94a6e1e26c2
Luís Lopes <maarnotto@gmail.com>
2022-11-19 18:05:19 +0000

    much improved version of spectrumAnalyser
    now searches in batch of increments and selects best_frequency, after the batch is finished the gap is narrowed and the precision is increased.

--- spectrumAnalyser.ino
+++ spectrumAnalyser.ino
@@ -8,28 +8,37 @@ void spectrumAnalyser(){ //sweep 425-450MHz and find highest frequency.
   float lower_limit = 425.000;
   float upper_limit = 450.000;
   float increment = 1;
+  float best_frequency = (upper_limit + lower_limit)/2; //center frequency of the search
   byte highest_rssi = 200;
   byte rssi_reading;
   int division = 10;
+  bool found = false;
   while(1){
     //spc_analyser.updateText(String(digitalRead(DIO2_R)), 40, 0, 1, NORMAL, 1);
     //spc_analyser.updateText(String(radio_R.readReg(REG_RSSIVALUE)), 0, 0, 1, NORMAL, 4);
-    
-    radio_R.setFrequencyMHz(current_frequency);
-    rssi_reading = radio_R.readReg(REG_RSSIVALUE);
-    Serial.print(current_frequency); Serial.print(" "); Serial.print(rssi_reading); Serial.print(" "); Serial.println(upper_limit - lower_limit);
-    if(rssi_reading < highest_rssi){
-      highest_rssi = rssi_reading;
-      spc_analyser.updateText(String(current_frequency, 3), 40, 20, 1, NORMAL, 8);
-      if (increment >= 0.001){ //shorten the search but increase precision
-        increment /= division; //divide increment to increase precision
-        upper_limit = current_frequency + ((upper_limit - lower_limit)/division); //decrease the gap
-        lower_limit = current_frequency - ((upper_limit - lower_limit)/division);
-      } 
+    highest_rssi = 200;
+    current_frequency = best_frequency - ((upper_limit - lower_limit)/2);
+    for(int i = 0; i < (upper_limit - lower_limit)/increment; i++){
+      current_frequency += increment;
+      radio_R.setFrequencyMHz(current_frequency);
+      //if(radio_R.readReg(REG_RSSICONFIG) == 2)
+        rssi_reading = radio_R.readReg(REG_RSSIVALUE);
+      Serial.print(current_frequency); Serial.print(" "); Serial.print(rssi_reading); Serial.print(" "); Serial.println(upper_limit - lower_limit);
+      if(rssi_reading < highest_rssi){
+        highest_rssi = rssi_reading;
+        best_frequency = current_frequency;
+        found = true;
+      }
     }
+    spc_analyser.updateText(String(best_frequency, 3), 40, 20, 1, NORMAL, 8);
+    if (increment >= 0.001 && found){ //shorten the search but increase precision
+      increment /= division; //divide increment to increase precision
+      upper_limit = best_frequency + ((upper_limit - lower_limit)/division); //decrease the gap
+      lower_limit = best_frequency - ((upper_limit - lower_limit)/division);
+    } 
     
-    current_frequency += increment;
-    if(current_frequency >= upper_limit) current_frequency = lower_limit;
+    
+    
     
     if(spc_analyser.clickB()){
 
