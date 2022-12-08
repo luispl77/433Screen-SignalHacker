c2e8f72748c36b885c77c5a2a92df11cae61cd2c
Luís Lopes <maarnotto@gmail.com>
2022-12-08 11:16:44 +0000

    fixed initialization of the radio

--- rc_car.ino
+++ rc_car.ino
@@ -1,11 +1,5 @@
 void carMode(){
-  unselect_R(); unselect_SD();
-  radio_T.initialize();
-  radio_T.setMode(RF69OOK_MODE_STANDBY);
-  radio_T.setModulationType(MOD_OOK);
-  radio_T.setFrequencyMHz(frequency_ISM);
-  radio_T.initializeTransmit(21, PA_MODE_PA1_PA2_20dbm, OCP_OFF);
-  
+  initialize_radio_car();
   car_mode.drawText("A: right", 0, 0, 1, NORMAL);
   car_mode.drawText("B: left", 0, 10, 1, NORMAL);
   car_mode.drawText("UP: forward", 0, 20, 1, NORMAL);
@@ -43,26 +37,32 @@ void carMode(){
 
 
 void speed50(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(2750);
 }
 
 void speed60(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(3250);
 }
 
 void speed70(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(3750);
 }
 
 void speed80(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(4250);
 }
 
 void speed90(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(4750);
 }
 
 void speed100(){
+  initialize_radio_car();
   for(int i = 0; i < 20; i++) pulse(5250);
 }
 
@@ -71,4 +71,13 @@ void pulse(int duration) {
   delayMicroseconds(duration);
   digitalWrite(DIO2_T, LOW);
   delayMicroseconds(duration/10);
+}
+
+void initialize_radio_car(){
+  unselect_R(); unselect_SD();
+  radio_T.initialize();
+  radio_T.setMode(RF69OOK_MODE_STANDBY);
+  radio_T.setModulationType(MOD_OOK);
+  radio_T.setFrequencyMHz(frequency_ISM);
+  radio_T.initializeTransmit(21, PA_MODE_PA1_PA2_20dbm, OCP_OFF);
 }
