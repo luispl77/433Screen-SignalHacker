b57d4fbf35a23be80d8ff93437553764ffc5f707
Luís Lopes <maarnotto@gmail.com>
2022-12-11 16:58:08 +0000

    finished rec n rep

--- RecordNReplay.ino
+++ RecordNReplay.ino
@@ -2,23 +2,21 @@
  unsigned long readings[10000];
 void recordReplay(){
   unsigned long previous_micros;
-  int i = 0;
+  int i = 0; int j = 0;
   int nr_samples = 0;
   unselect_T();
   unselect_SD();
-  radio_R.receiveBegin();
-  radio_R.initializeReceive();
-  unselect_R();
-  radio_T.setModulationType(MOD_OOK);
-  radio_T.setFrequencyMHz(433.92); 
-  radio_T.initializeTransmit(15, PA_MODE_PA1_PA2_20dbm, OCP_OFF);
-  rec_rep.drawText("SPL:", 0, 0, 1, NORMAL);
+  rec_rep.drawText("PULSES:", 0, 0, 1, NORMAL);
+  rec_rep.drawText("A: record", 60, 35, 1, NORMAL);
+  rec_rep.drawText("UP: replay", 60, 45, 1, NORMAL);
+  rec_rep.drawText("DOWN: clear", 60, 55, 1, NORMAL);
   delay(500);
-  for(int j = 0; j < ram; j++)readings[j] = 0;
+  
   while(1){
     if(rec_rep.clickA() && i < ram){
+      radio_R.receiveBegin(); rec_rep.updateText("REC..", 0, 40, 1, NORMAL, 8);
       ledOn(); i = 0;
-      while(rec_rep.clickA()){
+      while(rec_rep.clickA() && i < ram){
         if(digitalRead(DIO2_R) == HIGH){
           previous_micros = micros();
           while(digitalRead(DIO2_R) == HIGH);
@@ -29,59 +27,56 @@ void recordReplay(){
           i += 2;
         }       
       }
-      rec_rep.updateText(String(i), 40, 0, 1, NORMAL, 8);
-      delay(300);
+      rec_rep.updateText(String(i), 45, 0, 1, NORMAL, 8); //update pulses
+      delay(100);
       ledOff();
+      i = 0;
+      radio_R.receiveEnd();
+      radio_R.initializeTransmit(15, PA_MODE_PA1_PA2_20dbm, OCP_OFF);
+      radio_R.transmitBegin();
+      rec_rep.updateText("      ", 0, 40, 1, NORMAL, 8);
     }
 
     
     if(rec_rep.clickUP()){
-      i = 0;
+      j = 0; rec_rep.updateText("REP...", 0, 40, 1, NORMAL, 8);
       ledOn();
-      while(rec_rep.clickUP() && i < ram){
-        radio_T.send(1);
-        smartdelay(readings[i]);
-        radio_T.send(0);
-        smartdelay(readings[i+1]);
-        i += 2; 
+      while(rec_rep.clickUP()){
+        radio_R.send(1);
+        smartdelay(readings[j]);
+        radio_R.send(0);
+        smartdelay(readings[j+1]);
+        j += 2; 
+        if(j >= i) j = 0; //start from begining of recorded signal
       }
-      
-      delay(200);
+      rec_rep.updateText("      ", 0, 40, 1, NORMAL, 8);
+      delay(100);
       ledOff();
+      
     }
     
     if(rec_rep.clickDOWN()){
-      
+      ledOn();
+      clear_ram();
       delay(200);
+      ledOff();
 
     }
     
     if(rec_rep.clickB()){
-
+      radio_R.transmitEnd();
       break;
     }
   }
 }
 
-bool high(){
-  if(digitalRead(DIO2_R) == HIGH){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
 
-bool low(){
-  if(digitalRead(DIO2_R) == LOW){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
 
 void smartdelay(unsigned long micro){
   delay(micro/1000);
   delayMicroseconds(micro%1000);
+}
+
+void clear_ram(){
+  for(int j = 0; j < ram; j++)readings[j] = 0;
 }
